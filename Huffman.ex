5f0434300aa8df745c49ac3ffe1b5896aa75f201
Jonasdad <Jonasdad@kth.se>
2023-03-07 16:50:14 +0100

    ok i dont understand again

--- Huffman.ex
+++ Huffman.ex
@@ -8,13 +8,13 @@ defmodule Huffman do
   end
 
   def text() do
-    'ABCA'
+    'I am a little bird that likes worms and flying around!'
   end
 
   def test() do
     sample = text()
-    _tree = tree(sample)
-    #encode = encode_table(tree)
+    tree = tree(sample)
+    encode = encode_table(tree)
     #decode = decode_table(tree)
     #text = text()
     #seq = encode(text, encode)
@@ -30,33 +30,37 @@ defmodule Huffman do
   # Två metoder: huffman_tree och insert
   # huffman tree bygger trädet
   # build sätter rätt löv och noder på rätt plats
-  def huffman_tree([{tree, _n}]) do tree end
+  def huffman_tree([{tree, n}]) do tree end
   def huffman_tree([{char1, freq1}, {char2, freq2} | t]) do huffman_tree(build({{char1, char2}, freq1+freq2}, t)) end
+
+  # basecase
   def build({char, freq}, []) do [{char,freq}] end
+  # build the tree bottom - up. More frequent characters put higher up.
+  # ádding frequencies of low frequent characters should equal node to the left.
   def build({char1,freq1}, [{char2,freq2} | t]) do
     if(freq1 < freq2) do [{char1, freq1}, {char2, freq2} | t]; else[{char2, freq2} | build({char1, freq1}, t)] end
   end
 
+
+  def encode_table(tree) do
+    compress(tree)
+  end
   def compress({left, right}) do compress({left, right}, []) end
+  # Create code for each character BACKWARDS
   def compress({left, right}, sequence) do
+    left_seq = compress(left, [0 | sequence])
+    right_seq = compress(right, [1 | sequence])
+    left_seq ++ right_seq
+  end
+  # Generate the proper code for each character, but re-reverse the code
+  # to get the actual code.
+  def compress(char, code) do
+    [{char, Enum.reverse(code)}]
+  end
+
+  def generate_sequence([h|t]) do
 
   end
-  #def encode_table(tree) do
-    # To implement...
-  #end
-
-  #def decode_table(tree) do
-    # To implement...
-  #end
-
-  #def encode(text, table) do
-    # To implement...
-  #end
-
-  #def decode(seq, tree) do
-    # To implement...
-  #end
-
   # Initial call of freq/1 to initiate freq/2 using only the sample text
   def freq(sample) do freq(sample, []) end
 
